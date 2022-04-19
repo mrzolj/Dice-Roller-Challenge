87c35e802c8be5e97c6071d466902bf29da669e0
mrzolj <90713345+mrzolj@users.noreply.github.com>
2022-04-19 02:06:15 +0200

    Add files via upload

---
+++ Dice-Roller-Challenge-main/Dice roller challenge/Dice roller challenge/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+       
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fr2 = new Form2();
+            this.Hide();
+            fr2.Show();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
